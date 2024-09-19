f1f1feb5a2a197d6feb3dd95d6f7440e35435dff
unknown <andersolind@gmail.com>
2024-09-18 22:51:40 -0400

    init

---
+++ src/components/auto-complete.tsx
@@ -0,0 +1,105 @@
+import react, { ChangeEvent, FC, useState } from "react";
+import styled from "styled-components";
+import { FaArrowDown } from "@react-icons/all-files/fa/FaArrowDown";
+
+import {
+  AutoCompleteContainer,
+  AutoCompleteIcon,
+  Input,
+  AutoCompleteItem,
+  AutoCompleteItemButton
+} from "../style";
+
+const Root = styled.div`
+  position: relative;
+  width: 320px;
+`;
+
+interface IData {
+  name: string;
+  code: string;
+}
+interface autoCompleteProps {
+  iconColor?: string;
+  inputStyle?: react.CSSProperties;
+  optionsStyle?: react.CSSProperties;
+
+  data: any[];
+}
+export const AutoComplete: FC<autoCompleteProps> = ({
+  iconColor,
+  inputStyle,
+  optionsStyle,
+  data
+}) => {
+  const [search, setSearch] = useState({
+    text: "",
+    suggestions: []
+  });
+  const [isComponentVisible, setIsComponentVisible] = useState(true);
+
+  const onTextChanged = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    let suggestions:any = [];
+    if (value.length > 0) {
+      const regex = new RegExp(`^${value}`, "i");
+      suggestions = data.sort().filter((v: IData) => regex.test(v.name));
+    }
+    setIsComponentVisible(true);
+    setSearch({ suggestions, text: value });
+  };
+
+  const suggestionSelected = (value: IData) => {
+    setIsComponentVisible(false);
+
+    setSearch({
+      text: value.name,
+      suggestions: []
+    });
+  };
+
+  const { suggestions } = search;
+
+  return (
+    <Root>
+      <div
+        onClick={() => setIsComponentVisible(false)}
+        style={{
+          display: isComponentVisible ? "block" : "none",
+          width: "200vw",
+          height: "200vh",
+          backgroundColor: "transparent",
+          position: "fixed",
+          zIndex: 0,
+          top: 0,
+          left: 0
+        }}
+      />
+      <div>
+        <Input
+          id="input"
+          autoComplete="off"
+          value={search.text}
+          onChange={onTextChanged}
+          type={"text"}
+          style={inputStyle}
+        />
+  
+      </div>
+      {suggestions.length > 0 && isComponentVisible && (
+        <AutoCompleteContainer style={optionsStyle}>
+          {suggestions.map((item: IData) => (
+            <AutoCompleteItem key={item.code}>
+              <AutoCompleteItemButton
+                key={item.code}
+                onClick={() => suggestionSelected(item)}
+              >
+                {item.name}
+              </AutoCompleteItemButton>
+            </AutoCompleteItem>
+          ))}
+        </AutoCompleteContainer>
+      )}
+    </Root>
+  );
+};
